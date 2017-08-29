a5ab49c138ed82ec38943e06949602e53a2ed54b
Ayffi Nawaz <ayffin@gmail.com>
2017-08-29 18:16:23 -0400

    ang-checkpoint7 completed

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,67 @@
+(function() {
+  function SongPlayer() {
+    var SongPlayer = {};
+    /*
+     * @desc current selected song from song list
+     * @type {Object}
+     */
+    var currentSong = null;
+    /*
+     * @desc Buzz object audio file
+     * @type {Object}
+     */
+    var currentBuzzObject = null;
+    /*
+     * @function setSong
+     * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+     * @param {Object} song
+     */
+    var setSong = function(song) {
+      if (currentBuzzObject) {
+        currentBuzzObject.stop();
+        currentSong.playing = null;
+      }
+
+      currentBuzzObject = new buzz.sound(song.audioUrl, {
+        formats: ['mp3'],
+        preload: true
+      });
+
+      currentSong = song;
+    };
+    /*
+     * @function play
+     * @desc play the current song which is clicked
+     * @param {Object} song
+     */
+    SongPlayer.play = function(song) {
+      if (currentSong !== song) {
+
+        setSong(song);
+        currentBuzzObject.play();
+        song.playing = true;
+
+      } else if (currentSong === song) {
+        if (currentBuzzObject.isPaused()) {
+          currentBuzzObject.play();
+        }
+      }
+    };
+    /*
+     * @function pause
+     * @desc pause the current song
+     * @param {Object} song
+     */
+    SongPlayer.pause = function(song) {
+      currentBuzzObject.pause();
+      song.playing = false;
+    };
+
+    return SongPlayer;
+  }
+
+
+  angular
+    .module('blocJams')
+    .factory('SongPlayer', SongPlayer);
+})();
